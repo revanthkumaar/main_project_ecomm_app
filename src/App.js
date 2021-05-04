0bbb8f7d6e0bcca2459a7330eef99c141311ffbe
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-04 14:03:22 +0530

    ashdl
    qelkasld

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
 import React from 'react';
+//ROUTING DEPENDENCEIES 
 import { Link, Switch, Route } from 'react-router-dom';
-
 import './App.css';
 
+//THESE ARE THE PAGES IN THIS APP
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
-const TopicDetail = props => {
-    console.log(props)
-
-  return(
-
-    <div>
-    <h1>topic detail page of: {props.match.params.topicId}</h1>
-  </div>
-
-  );
- 
-  };
-
-const TopicsList = props => {
-
-  return(
-    <div>
-    <h1>topics list</h1>
-
-    
-    <Link to={`${props.match.url}/covid19`}>Increased cases in India</Link>
-    <Link to={`${props.match.url}/sars20`}>sars</Link>
-    <Link to={`${props.match.url}/malaria`}>malaria</Link>
-  </div>
-  )
-  
-
-};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/topics' component={TopicsList} />
-        <Route path='/topics/:topicId' component={TopicDetail}/>
-        
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
