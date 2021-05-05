78838ed39f89276b7b110cb14893742f83f07dc4
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-05 16:06:36 +0530

    asdjh
    lqkwlaskjdlas

--- src/App.js
+++ src/App.js
@@ -44,19 +44,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={ () => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser:user.currentUser
-}
-
-)
-
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-
-const CartIcon = () => (
-
-    <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon'/>
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className='cart-icon' onClick={toggleCartHidden}>
+    <ShoppingIcon className='shopping-icon' />
     <span className='item-count'>0</span>
-
-    </div>
+  </div>
 );
 
-
 const mapDispatchToProps = dispatch => ({
-   toggleCartHidden: () => dispatch(toggleCartHidden())
-  });
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(
+  null,
+  mapDispatchToProps
+)(CartIcon);
