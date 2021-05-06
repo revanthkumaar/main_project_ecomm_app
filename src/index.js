45aea2ca40bae914c1443afc671c953864c66659
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-06 15:03:03 +0530

    lasdlsajd
    asldjkasldkjas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import store from './redux/store';
+//local storage
+import {PersistGate} from 'redux-persist/integration/react'
+import {store,persistor} from './redux/store'
+
+
 
 import './index.css';
 import App from './App';
@@ -11,7 +15,10 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
+    
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,21 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+//this is for local/session storage
+import {persistReducer} from 'redux-persist';
 
-export default combineReducers({
+import storage from 'redux-persist/lib/storage'; //local storage
+//import sessionStorage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key:'root',
+  storage, 
+  whitelist:['cart','user']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig,rootReducer)
