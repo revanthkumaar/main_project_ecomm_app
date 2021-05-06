9a0e972d14aae76f9a4cb041349da4652d3393c9
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-06 16:13:15 +0530

    askjd
    jqlaksjdl

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -8,15 +8,18 @@ import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; //local storage
 //import sessionStorage from 'redux-persist/lib/storage'
 
+import directoryReducer from './directory/directory.reducer'
+
 const persistConfig = {
   key:'root',
   storage, 
-  whitelist:['cart','user']
+  whitelist:['cart','directory']
 }
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory:directoryReducer
 });
 
 export default persistReducer(persistConfig,rootReducer)
